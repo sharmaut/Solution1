1c3ce95730834cfe635704dbd820bca2f70cb5f4
sharmaut <40994950+sharmaut@users.noreply.github.com>
2018-09-05 00:30:26 +1000

    Add files via upload

---
+++ sketch_sep05a.ino
@@ -0,0 +1,16 @@
+void setup() {
+  // put your setup code here, to run once:
+pinMode(2, INPUT); //Pin 2 as INPUT
+pinMode(3, OUTPUT); //PIN 3 as OUTPUT
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+if (digitalRead(2) == HIGH) // check if PIR is triggered
+{
+  digitalWrite(3, HIGH); // turn the LED/Buzz ON
+  delay(300);            // wait for 0.3 second
+  digitalWrite(3, LOW);  // turn the LED/Buzz OFF
+  delay(300);            // wait for 0.3 second 
+}
+}
